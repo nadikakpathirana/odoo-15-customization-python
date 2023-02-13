24df36c3c003a4e991a160c532c532e97f1a08ea
nadikakpathirana <nadikakpathirana@gmail.com>
2023-02-13 15:11:38 +0530

    Added menus and submenus for the remedy hospital module.

--- _custom_addons/remedy_hospital/__manifest__.py
+++ _custom_addons/remedy_hospital/__manifest__.py
@@ -6,7 +6,9 @@
     'summary': 'test summery',
     'description': """ test description """,
     'depends': [],
-    'data': [],  # use to import xml files
+    'data': [
+        'views/menu.xml'
+    ],  # use to import xml files
     'demo': [],  # use to import demo data into the system
     'application': True,  # so that module is the main domain(That is a application by itself)
     'auto_install': False,
