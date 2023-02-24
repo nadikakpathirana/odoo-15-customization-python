03fff74a17d8954cd2adb0088526549be183a135
nadikakpathirana <nadikakpathirana@gmail.com>
2023-02-24 16:33:26 +0530

    om_hospital module added.

--- _custom_addons/committee_management/__manifest__.py
+++ _custom_addons/committee_management/__manifest__.py
@@ -7,11 +7,12 @@
     'description': """ 
     The Committee Management Module manages the critical administrative processes such as setting up meetings for envelops opening, technical evaluations, advisory committees and board of counselors and any operations related to business. This module allows to establish committees, hold them with qualified members, create and schedule meetings and issue reports, minutes and decisions made. This module, with the help of steering committees assists to lead patient complaint management operations. 
     """,
-    'depends': [],
+    'depends': ['crede_complainer_registration', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
-        'views/committee_view.xml'
+        'views/committee_view.xml',
+        'views/complainer_view.xml',
     ],  # use to import xml files
     'demo': [],  # use to import demo data into the system
     'application': True,  # so that module is the main domain(That is a application by itself)
